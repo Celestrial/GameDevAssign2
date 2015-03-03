84133277a3ad6b4421e592f93d82ef8bcf9ed989
celestrial <sean.celestrial@gmail.com>
2015-03-03 01:09:45 -0500

    Added gem to follow paths, able to select start and end path. Transform of nodes are stored and color changed. red start green destination. bool onTheMove to prevent clicking while moving.

---
+++ Q3/Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	bool firstClick = true;
+	bool onTheMove = false;
+	Transform startPos;
+	Transform endPos;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!onTheMove)
+		{
+			if (Input.GetButtonDown("Fire1")) {
+				RaycastHit hit;
+				Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+				if (Physics.Raycast(ray, out hit))
+				{
+					if(firstClick && hit.collider.tag == "node")
+					{
+						transform.position = hit.transform.position;
+						transform.renderer.enabled = true;
+						hit.transform.renderer.material.color = Color.red;
+						startPos = hit.transform;
+						firstClick = false;
+					}else{
+						hit.transform.renderer.material.color = Color.green;
+						endPos = hit.transform;
+						onTheMove = true;
+					}
+				}
+			}
+		}
+	}
+}
